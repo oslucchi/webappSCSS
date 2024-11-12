35f4ba632a4d9ba748551f9c2cc742a81987ef39
Osvaldo Lucchini <osvaldo.lucchini@gmail.com>
2024-11-12 19:02:55 +0100

    adjusted presentation of shipments

--- src/app/_components/ardex/ardex-receiving/ardex-receiving.component.ts
+++ src/app/_components/ardex/ardex-receiving/ardex-receiving.component.ts
@@ -78,6 +78,7 @@ export class ArdexReceivingComponent implements OnInit {
   private palletsDisplayedColumns: any[] = [
     { def: "idPallet", hide: false },
     { def: "barcode", hide : false},
+    { def: "weight", hide : false},
     { def: "checked", hide: false},
     { def: "stocked", hide: false}
   ];
--- src/app/_models/ardex-refill-pallet.ts
+++ src/app/_models/ardex-refill-pallet.ts
@@ -4,6 +4,7 @@ export class ArdexRefillPallet {
     idPallet: number = 0;
 	idShipment: number = 0;
 	barcode: string = "";
+	weight: number = 0;
 	articles: ArdexRefillArticle[] = [];
 	checked: boolean = false;
 	stocked: boolean = false;
