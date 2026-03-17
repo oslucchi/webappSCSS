805c37b944cea289a97eed9a79eea7c040841de6
Osvaldo Lucchini <osvaldo.lucchini@gmail.com>
2026-03-17 08:05:00 +0100

    change of LAN address

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  CHAT_URL: "ws://192.168.60.184:8080/orderMngrAX/endpoint",
-  host: "http://192.168.60.184:8080",
+  CHAT_URL: "ws://10.130.231.184:8080/orderMngrAX/endpoint",
+  host: "http://10.130.231.184:8080",
   baseURL: "/orderMngrAX",
   baseHref: "/orderMngrAX/"
 };
